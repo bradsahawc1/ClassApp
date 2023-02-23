6d0e40fb8b329253e97d11cc32cd8c56ae38b7e0
bradshawc1 <98545738+bradsahawc1@users.noreply.github.com>
2023-02-23 18:46:46 -0500

    adding files to github

---
+++ app/src/main/java/com/example/classapp/Holomember.kt
@@ -0,0 +1,13 @@
+package com.example.classapp
+
+data class Holomember(
+    val image: String,
+    val name: String,
+    val subscribers: Int,
+    val gen: String,
+    val birthday: String,
+    val debut: String,
+    val illustrator: String,
+    val status: String,
+    val description: String
+)
